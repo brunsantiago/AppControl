be8cb065efd599861114b6881658c8c622313184
Santiago Brun <brunsantiago@hotmail.com>
2023-02-02 09:09:16 -0300

    Change API_PATH

--- app/src/main/java/com/example/sata/myapplication2/Configurador.java
+++ app/src/main/java/com/example/sata/myapplication2/Configurador.java
@@ -9,7 +9,8 @@ public class Configurador {
 
     //public static final String API_PATH = "http://192.168.1.8:3000/api/"; // Dev Local
     //public static final String API_PATH = "http://186.182.25.11:3000/api/"; // Pro Server Datamanager
-    public static final String API_PATH = "https://api-datamanager.click/api/"; //AMAZON AWS EC2 - app-server
+    //public static final String API_PATH = "https://api-datamanager.click/api/"; //AMAZON AWS EC2
+    public static final String API_PATH = "https://app-server.com.ar/api-dm/"; // PLESK - app-server.com.ar
 
     private Date finSesion;
 
